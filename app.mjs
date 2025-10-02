b19c4af6eed28bf02f844e641d413e349c892825
megacodist <megacodist@gmail.com>
2025-10-02 20:29:14 +0330

    Configuring the project

--- app.mjs
+++ app.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
     url: "https://github.com/rothsandro/eleventy-notes/edit/{{branch}}/{{file}}",
   },
   staticAssets: {
-    paths: { "public/": "/" },
+    paths: { "assets/": "/assets/" },
   },
   ignores: ["README.md"],
   customProperties: {
@@ -27,77 +27,12 @@ export default defineConfig({
   sidebar: {
     links: [
       {
-        url: "https://github.com/rothsandro/eleventy-notes",
+        url: "https://github.com/megacodist/abali",
         label: "GitHub / Support",
         icon: "github",
       },
-      {
-        url: "https://www.buymeacoffee.com/sandroroth",
-        label: "Buy me a coffee",
-        icon: "coffee",
-      },
     ],
     sections: [
-      {
-        label: "Introduction",
-        groups: [
-          {
-            query: createNotesQuery({
-              pattern: "^/[^/]+$",
-              tags: ["basics"],
-            }),
-          },
-        ],
-      },
-      {
-        label: "Guides",
-        groups: [
-          {
-            label: "Writing Notes",
-            query: createNotesQuery({
-              pattern: "^/Writing/",
-              tree: {
-                replace: {
-                  "^/\\w+": "",
-                },
-              },
-            }),
-          },
-          {
-            label: "Organizing Notes",
-            query: createNotesQuery({
-              pattern: "^/Organizing/",
-            }),
-          },
-          {
-            label: "Core Features",
-            query: createNotesQuery({
-              pattern: "^/Features/",
-              tree: {
-                replace: {
-                  "^/\\w+": "",
-                },
-              },
-            }),
-          },
-          {
-            label: "Deployment",
-            query: createNotesQuery({
-              pattern: "^/Deployment/",
-            }),
-          },
-        ],
-      },
-      {
-        label: "Releases",
-        groups: [
-          {
-            query: createNotesQuery({
-              pattern: "^/Releases/",
-            }),
-          },
-        ],
-      },
     ],
   },
   tags: {
